2c52f6ea1e5bbcc47954acb90234c11d1241e9f2
Darius Moore <iamdariusmoore@gmail.com>
2023-09-05 17:07:53 -0400

    stater code for user controller

--- backend/controllers/users-controller.js
+++ backend/controllers/users-controller.js
@@ -1,16 +1,45 @@
 const HttpError = require("../models/http-error");
 const {v4: uuidv4 } = require("uuid");
 
-const getAllUsers = (request, response) => {
+const DUMMY_USERS = [
+    {
+        id: 'u1',
+        name: 'Test user',
+        email: 'testuser@test.com',
+        password: 'testers'
+    }
+];
+
+const getAllUsers = (request, response, next) => {
+
+    response.json({'users': DUMMY_USERS})
+};
+
+const createNewUser = (request, response, next) => {
+    const { name, email, password } = request.body;
+
+    const newUser = {
+        id: uuidv4(),
+        name,
+        email,
+        password
+    };
+
+    DUMMY_USERS.push(newUser);
+    response.json({ 'userAdded': newUser });
 
 };
 
-const createNewUser = (request, response) => {
+const loginUser = (request, response, next) => {
+    const { email, password } = request.body;
 
-};
+    const userFound = DUMMY_USERS.findIndex(user => user.email === email);
 
-const loginUser = (request, response) => {
+    if (!userFound || userFound.password === password) {
+        throw new HttpError('Could not identify user.', 401)
+    };
 
+    response.json({'message': 'user logged in'})
 };
 
 exports.getAllUsers = getAllUsers;
