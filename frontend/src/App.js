3057340593555d4092c8354cc9f741d8f623a5aa
Darius Moore <iamdariusmoore@gmail.com>
2023-09-07 12:18:31 -0400

    updating routes for places page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
+
 import Users from './users/pages/User';
+import NewPlace from './places/pages/NewPlace';
+
 const App = () => {
     return <Router>
-        <Route path="/">
-            <Users />
-        </Route>
+        <Switch>
+            <Route path="/" exact>
+                <Users />
+            </Route>
+            <Route path="/places/new" exact>
+                <NewPlace />
+            </Route>
+            <Redirect to="/" />
+        </Switch>
     </Router>;
 }
 
